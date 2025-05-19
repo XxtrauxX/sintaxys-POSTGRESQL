4061345b17a54f28f2ee95e4524ebecb66177a00
Oscar Alejandro Díaz Ojeda <aaronjones2429@gmail.com>
2025-05-19 13:29:22 -0500

    Create Sintaxis.sql

---
+++ Sintaxis.sql
@@ -0,0 +1,10 @@
+-- FORMA CORRECTA DEL INSERT
+
+insert into funcion values('1','oscar'),('2','hulio'),('3','gabriel')
+
+
+
+-- FORMA CORRECTA DEL UPDATE
+
+update funcion set idfuncion = '5'
+where nombre is null
